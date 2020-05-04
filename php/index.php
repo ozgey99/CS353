eac9ffb194155519119f277ea9d964164d47b724
Ozge Yasayan <ozgey99@gmail.com>
2020-05-04 11:45:30 +0300

    main updated

---
+++ php/index.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+        <title>Footballer Scouting Application</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </head>
+
+    <h1 style="color: whitesmoke; alignment: center; font-family: Verdana; font-size: xx-large">
+        Footballer Scouting Application
+    </h1>
+
+    <body style="background-color: #1d2124; background-image: url('football.jpg'); background-size: cover; color: antiquewhite; alignment: center; font-family: Verdana; font-size: large">
+        <p>Looking to add new players to your team? Sign up for free as a club.</p>
+        <p>You can make and respond to transfer offers, view reports and information about footballers.</p>
+        <p>You can also sign up as an agency/scout in order to submit reports on footballers.</p>
+    </body>
+
+    <div class="row" style="color: #1d2124">
+        <div class="col-md-2 ml-md-auto" align="center">
+            <button type="button" class="btn btn-outline-light btn-lg" href="login.php">
+                Login
+            </button>
+        </div>
+        <div class="col-md-2 ml-md-auto">
+            <button type="button" class="btn btn-outline-light btn-lg" href="signup.php">
+                Sign up
+            </button>
+        </div>
+    </div>
+
+    <footer class="card-footer">
+        This website is developed as a CS353 project by Fatih Çakır, Rabia Nur Önal, Özge Yaşayan, Celal Bayraktar.
+    </footer>
+
+</html>
