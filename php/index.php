f058ec44cd908258d3876db28956acfa50f85237
Ozge Yasayan <ozgey99@gmail.com>
2020-05-17 16:38:46 +0300

    index updated

--- php/index.php
+++ php/index.php
@@ -19,17 +19,28 @@
         <p>Looking to add new players to your team? Sign up for free as a club.</p>
         <p>You can make and respond to transfer offers, view reports and information about footballers.</p>
         <p>You can also sign up as an agency/scout in order to submit reports on footballers.</p>
+        <p>If you're a journalist, you can subscribe to teams and keep up with the latest transfers.</p>
     </body>
 
     <div class="row" style="color: #1d2124">
         <div class="col-md-2 ml-md-auto" align="center">
-            <button type="button" class="btn btn-outline-light btn-lg" href="login.php">
+            <button type="button" class="btn btn-outline-light btn-lg" onclick="window.location.href='/login.php'">
                 Login
             </button>
         </div>
-        <div class="col-md-2 ml-md-auto">
-            <button type="button" class="btn btn-outline-light btn-lg" href="signup.php">
-                Sign up
+        <div class="col-md-3 ml-md-auto">
+            <button type="button" class="btn btn-outline-light btn-lg" onclick="window.location.href='/signup_journalist.php'">
+                Sign up as a journalist
+            </button>
+        </div>
+        <div class="col-md-3 ml-md-auto">
+            <button type="button" class="btn btn-outline-light btn-lg" onclick="window.location.href='/signup_agency.php'">
+                Sign up as an agency
+            </button>
+        </div>
+        <div class="col-md-3 ml-md-auto">
+            <button type="button" class="btn btn-outline-light btn-lg" onclick="window.location.href='/signup_agent.php'">
+                Sign up as an agent
             </button>
         </div>
     </div>
