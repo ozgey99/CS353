dbb00efc7a6b322895cfedc1f5b475e1db732f22
cakirfatih <55812706+cakirfatih@users.noreply.github.com>
2020-05-18 01:38:18 +0300

    Update home_agency.php

--- php/home_agency.php
+++ php/home_agency.php
@@ -9,11 +9,11 @@
     <h2>Welcome to Agency Home Page</h2>
 
     <?php 
-    	if (isset($_SESSION['userId'])) {
+    	if (isset($_SESSION['id'])) {
     		echo "<p class='login-status'>You are logged in as Agency!</p>";
     	}
     ?>
-    <form action="includes/logout_inc.php">
+    <form action="logout_inc.php">
 
     	<button type="submit" name="logout-submit">Logout</button>
 
