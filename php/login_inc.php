8860adbb33fdc4af4859838ab619a5e3ed8fba48
Celal Bayraktar <56126608+CelalBayraktar@users.noreply.github.com>
2020-05-18 01:10:30 +0300

    Update login_inc.php

--- php/login_inc.php
+++ php/login_inc.php
@@ -40,19 +40,21 @@ if(isset($_POST['login-submit'])){
 
 		 	else {
 
-		 		$sql_select1 = "SELECT * FROM agency WHERE username = '$uid';";
+		 		$userId = $row['id']; 
+
+				$sql_select1 = "SELECT id FROM agency WHERE id = '$userId';";
 		 		$result1 = mysqli_query($conn, $sql_select1);
 		 		$resultCheck1 = mysqli_num_rows($result1);
-		 		$sql_select2 = "SELECT * FROM agent WHERE username = '$uid';";
+		 		$sql_select2 = "SELECT id FROM agent WHERE id = '$userId';";
 		 		$result2 = mysqli_query($conn, $sql_select2);
 		 		$resultCheck2 = mysqli_num_rows($result2);
-		 		$sql_select3 = "SELECT * FROM club WHERE username = '$uid';";
+		 		$sql_select3 = "SELECT id FROM club WHERE id = '$userId';";
 		 		$result3 = mysqli_query($conn, $sql_select3);
 		 		$resultCheck3 = mysqli_num_rows($result3);
-		 		$sql_select4 = "SELECT * FROM journalist WHERE username = '$uid';";
+		 		$sql_select4 = "SELECT id FROM journalist WHERE id = '$userId';";
 		 		$result4 = mysqli_query($conn, $sql_select4);
 		 		$resultCheck4 = mysqli_num_rows($result4);
-		 		$sql_select5 = "SELECT * FROM scout WHERE username = '$uid';";
+		 		$sql_select5 = "SELECT id FROM scout WHERE id = '$userId';";
 		 		$result5 = mysqli_query($conn, $sql_select5);
 		 		$resultCheck5 = mysqli_num_rows($result5);
 
