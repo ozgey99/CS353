8a356b61924dedd94e250c87484fbef2e1675b7c
Ozge Yasayan <ozgey99@gmail.com>
2020-05-18 13:53:34 +0300

    updated home_scout

--- php/home_scout.php
+++ php/home_scout.php
@@ -5,20 +5,24 @@
 <div class="container">
 
   <div >
-
+      <br>
     <h2>Welcome to Scout Home  Page</h2>
-
+      <br>
     <?php 
     	if (isset($_SESSION['id'])) {
     		echo "<p class='login-status'>You are logged in as Scout!</p>";
     	}
     ?>
+      <br>
     <form action="logout_inc.php">
 
     	<button type="submit" name="logout-submit">Logout</button>
 
     </form>
-    	
+      <br> <br>
+      <div>
+          <a href="scout_tasks.php" style="color: #0c5460">Want to see your tasks?</a>
+      </div>
 
   </div>
 
