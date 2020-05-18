fdbb02ee689d0f50b27aa67780b93f7b848ed1fe
Ozge Yasayan <ozgey99@gmail.com>
2020-05-18 13:45:17 +0300

    agency home changed

--- php/home_agency.php
+++ php/home_agency.php
@@ -4,21 +4,26 @@
 
 <div class="container">
 
-  <div >
+  <div>
 
+      <br>
     <h2>Welcome to Agency Home Page</h2>
-
+      <br>
     <?php 
     	if (isset($_SESSION['id'])) {
     		echo "<p class='login-status'>You are logged in as Agency!</p>";
     	}
     ?>
+      <br>
     <form action="logout_inc.php">
 
     	<button type="submit" name="logout-submit">Logout</button>
 
     </form>
-    	
+      <br> <br>
+      <div>
+          <a href="agency_requests.php" style="color: #0c5460">Want to see your requests?</a>
+      </div>
 
   </div>
 
