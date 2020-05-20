ac0486ae1fd350a2ea0a98715d99468f626d6cb3
Celal Bayraktar <56126608+CelalBayraktar@users.noreply.github.com>
2020-05-20 11:59:48 +0300

    Update home_club.php

--- php/home_club.php
+++ php/home_club.php
@@ -5,25 +5,35 @@
 <div class="container">
 
   <div >
-      <br>
+
     <h2>Welcome to Club Home Page</h2>
-      <br>
 
     <?php 
     	if (isset($_SESSION['id'])) {
-    		echo "<p class='login-status'>You are logged in as Club!</p>";
+    		echo "<p class='login-status'>You are logged in as ",$_SESSION['username'],"!</p>";
     	}
+      else {
+        echo "<p class='login-status'>You are logged out!</p>";
+      }
+
     ?>
-      <br>
     <form action="logout_inc.php">
 
     	<button type="submit" name="logout-submit">Logout</button>
 
     </form>
-      <br><br>
-      <div>
-          <a href="club_reports.php" style="color: #0c5460">Want to see the reports?</a>
-      </div>
+    
+    <br>
+
+    <form method="post">
+      <button type = "submit" name = "request">Request Scout</button>
+      <?php 
+        if(isset($_POST['request'])){
+          header("Location: scout_request.php");
+          exit();
+        }
+      ?>
+    </form>
 
   </div>
 
