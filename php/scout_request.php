e48ccf64704a7d313299a6c3796b5dc581dd0f6d
Celal Bayraktar <56126608+CelalBayraktar@users.noreply.github.com>
2020-05-20 04:31:23 +0300

    request scout page for club

---
+++ php/scout_request.php
@@ -0,0 +1,97 @@
+<?php 
+    require 'header.php';
+    include 'config.php';
+    $conn = $cn;
+?>
+<?php
+    $select_organization = "SELECT * FROM organizations;";
+    $result = mysqli_query($conn, $select_organization);
+    $organizations = array();
+    $resultCheck = mysqli_num_rows($result);
+    while ($row = mysqli_fetch_assoc($result)) {
+		$organizations[] = $row['organization_name'];
+	}
+?>
+<div class="container">
+
+    <div class="request">
+
+       <?php 
+       		if (!empty($_SESSION['id'])) {
+       			echo "<p class=error>Session Active</p>";
+       		}
+       ?>
+       <form action = "scout_request_inc.php" method = "post">
+			<label for="agency">Select an agency:</label>
+			<select name="agency">
+			  <?php
+			  	foreach ($_SESSION['agency_list'] as $agency) {
+			  ?>	
+			  <option><?php echo $agency;?></option>
+			  <?php } ?>
+			</select>
+
+			<br>
+
+			<label for="noOfScout">Number Of Scouts:</label>
+			<select name="noOfScout">
+				<?php
+				    for ($i=1; $i<=10; $i++)
+				    {
+				        ?>
+				            <option value="<?php echo $i;?>"><?php echo $i;?></option>
+				        <?php
+				    }
+				?>
+			</select>
+
+			<br>
+
+			<label for="organization">Select Organization:</label>
+			<select name="organization">
+				<?php
+				  	foreach ($organizations as $organization) {
+				?>	
+				<option><?php echo $organization;?></option>
+				<?php } ?>
+			</select>
+
+			<br>
+
+			<label for="due-date">Due Date</label>
+			<input type="date" name="end-date" value="<?php echo date("Y-m-d");?>">
+
+			<br>
+			<label>Choose position(s):</label>
+			<br>
+			<input type="checkbox" name="pos[]" value="gk">Goalkeeper(GK)<br>
+
+			<input type="checkbox" name="pos[]" value="rb">Right Back(RB)<br>
+
+			<input type="checkbox" name="pos[]" value="cb">Center Back(CB)<br>
+
+			<input type="checkbox" name="pos[]" value="lb">Left Back(LB)<br>
+			
+			<input type="checkbox" name="pos[]" value="dm">Defensive Midfielder(DM)<br>
+			
+			<input type="checkbox" name="pos[]" value="cm">Center Midfielder(CM)<br>
+
+			<input type="checkbox" name="pos[]" value="lm">Left Midfielder(LM)<br>
+
+			<input type="checkbox" name="pos[]" value="rm">Right Midfielder(RM)<br>
+
+			<input type="checkbox"  name="pos[]" value="cam">Center Attack Midfielder(CAM)<br>
+
+			<input type="checkbox" name="pos[]" value="lf">Left Forward(LF)<br>
+
+			<input type="checkbox" name="pos[]" value="rf">Right Forward(RF)<br>
+
+			<input type="checkbox" name="pos[]" value="cf">Center Forward(CF)<br>
+
+			<input type="checkbox" name="pos[]" value="st">Striker(ST)<br>
+			
+
+			<button type="submit" name="request-submit">Submit</button>
+		</form> 
+    </div>
+</div>
---
+++ php/scout_request_inc.php
@@ -0,0 +1,69 @@
+
+<?php
+
+	include 'config.php';
+	$conn = $cn;
+	session_start();
+
+	if (isset($_POST['request-submit']) && !empty($_SESSION['id'])) {
+
+		$club_name = $_SESSION['username'];
+		$sql_select = "SELECT * FROM club WHERE name = '$club_name';";
+		$result = mysqli_query($conn, $sql_select);
+		$resultCheck = mysqli_num_rows($result);
+		$positions = $_POST['pos'];
+
+		
+		if(!empty($_POST['agency']) && !empty($_POST['noOfScout']) && !empty($_POST['organization']) && sizeof($positions) > 0 && $resultCheck > 0){
+    		
+    		$selected_agency = $_POST['agency'];
+			$club_id = $_SESSION['id'];
+			$start_date = date('Y-m-d');
+			$end_date = $_POST['end-date'];
+    		$selected_no_of_scouts = $_POST['noOfScout'];
+    		$selected_organization = $_POST['organization'];
+			$sql_select_agency_id = "SELECT id FROM agency WHERE name = '$selected_agency';";
+			$agency_id_result = mysqli_query($conn, $sql_select_agency_id);
+			$agency_id_fetch = mysqli_fetch_assoc($agency_id_result);
+			$agency_id = $agency_id_fetch['id'];
+
+			if (mysqli_num_rows($agency_id_result) > 0) {
+
+				$insert_request = "INSERT INTO request (no_of_req_scouts, organization, start_date, end_date) 
+									VALUES ('$selected_no_of_scouts','$selected_organization', '$start_date', '$end_date');";
+				mysqli_query($conn, $insert_request);
+
+				$select_request = "SELECT MAX(id) AS id FROM request;";
+				$request_id_result = mysqli_query($conn, $select_request);
+				$request_id_fetch = mysqli_fetch_assoc($request_id_result);
+				$request_id = $request_id_fetch['id'];
+				$insert_requests = "INSERT INTO requests (request_id, club_id, agency_id) 
+									VALUES ('$request_id', '$club_id', '$agency_id');";
+				mysqli_query($conn, $insert_requests);
+
+
+				foreach($positions as $position) {
+
+					$insert_positions ="INSERT INTO request_positions (id, position) VALUES ('$request_id','$position');";
+                    mysqli_query($conn, $insert_positions);
+
+                }
+
+				header("Location: ../home_club.php");
+				exit();
+
+			}			
+    			
+		}
+
+		else{
+
+			header("Location:  ../scout_request.php?emptyForm");
+			exit();
+
+		}
+
+
+	}
+
+
