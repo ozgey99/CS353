561ed2c7cfcd9757b2af0bb2e87f52ae0c1fdf7c
cakirfatih <55812706+cakirfatih@users.noreply.github.com>
2020-05-20 23:33:57 +0300

    Update transferring.php

--- php/transferring.php
+++ php/transferring.php
@@ -6,8 +6,8 @@
         $footballer = $_POST['footballer'];
         $query = "UPDATE offers SET status='accepted' WHERE offerer_id = '$rejected_team' AND offeree_id = '$current_team' AND footballer_id = '$footballer' AND status='pending'";
         mysqli_query($cn,$query);
-        Header('Location: offers.php');
-        
+        header('Location: offers.php');
+        exit();
     }
     else if(isset($_POST['reject'])) {
         $rejected_team = $_POST['new_team'];
@@ -15,7 +15,8 @@
         $footballer = $_POST['footballer'];
         $query = "UPDATE offers SET status='rejected' WHERE offerer_id = '$rejected_team' AND offeree_id = '$current_team' AND footballer_id = '$footballer' AND status='pending'";
         mysqli_query($cn,$query);
-        Header('Location: offers.php');
+        header('Location: offers.php');
+        exit();
     }
     else
         echo "You should not be here!";
