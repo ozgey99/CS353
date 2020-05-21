dcbab92a8b21be9b6f3a5f87c84c63b7c64f5686
cakirfatih <55812706+cakirfatih@users.noreply.github.com>
2020-05-21 20:56:39 +0300

    Update firing.php

--- php/firing.php
+++ php/firing.php
@@ -4,11 +4,14 @@
         $query1 = "DELETE FROM scout_position_exp WHERE id = '$scout_id'";
         $query2 = "DELETE FROM scout_league_exp WHERE id='$scout_id'";
         $query3 = "DELETE FROM works WHERE scout_id = '$scout_id'";
-        $query4 = "DELETE FROM user WHERE id='$scout_id'";
+        $query4 = "DELETE FROM reports WHERE scout_id = '$scout_id'";
+        $query5 = "DELETE FROM user WHERE id='$scout_id'";
+
         mysqli_query($cn,$query1);
         mysqli_query($cn,$query2);
         mysqli_query($cn,$query3);
         mysqli_query($cn,$query4);
+        mysqli_query($cn,$query5);
         header("Location: scouts.php");
     }
 
