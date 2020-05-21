bd2fb824d9e3cd51b38f20f24fbd40119f6d5e0d
rabional <nur.onal@ug.bilkent.edu.tr>
2020-05-21 16:00:00 +0300

    club rapor görüntüleme

--- php/club_reports.php
+++ php/club_reports.php
@@ -13,23 +13,41 @@
 include 'config.php';
 
 $uid = $_SESSION['id'];
-$sql_select = "select footballer.name, scout.name, report.rating, report.comment, report.date
-               from report, footballer_report, footballer, scout
-               where report.scout_id = scout.id and report.id = footballer_report.report_id
-               and footballer_report.footballer_id = footballer.id and report.club_id = '$uid';
-";
+$sql_select = "select report.id as reportID, report.date as date, report.rating as rating, report.comment as comment, 
+                scout.name as scout, footballer.name footballer 
+                from report, reports, footballer_report, scout, club, footballer
+                where report.id = reports.report_id 
+                and report.id = footballer_report.report_id
+                and reports.scout_id = scout.id
+                and club.id = reports.club_id
+                and club.id = '$uid'
+                and footballer_report.footballer_id = footballer.id
+                order by report.date desc";
 
 $result = mysqli_query($cn, $sql_select);
 
-if ($result->num_rows > 0) {
-    echo "<table><tr><th>Footballer</th><th>Scout</th><th>Rating</th><th>Comment</th><th>Date</th></tr>";
+if (mysqli_num_rows($result) > 0) {
+    echo "<form name='form' action='view_report.php' method='post'>
+        <table><tr><th>Footballer</th><th>Scout</th><th>Rating</th><th>Comment</th><th>Date</th></tr>";
 
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["footballer.name"]."</td><td>".$row["scout.name"]."</td><td>
-             ".$row["report.rating"]."</td><td>".$row["report.comment"]."</td><td>".$row["report.date"]."</td></tr>";
+        echo "<tr><td>".$row["footballer"]."</td><td>".$row["scout"]."</td><td>
+             ".$row["rating"]."</td><td>".substr($row["comment"],0, 20)."..."."</td><td>".$row["date"]."</td> <td>
+             <input type='radio' name='select' value=".$row['reportID'] ."></td> </tr>";
+
     }
 
-    echo "</table>";
+    echo "</table> <br><button type='submit' class='button' name='view' id='view'>View</button> 
+  </form>";
+?>
+    <br> <button type='button' class='button' onclick="window.location.href='/cs353/php/transfer_offer.php'">
+    Transfer Offer Page
+    </button>
+    <br> <button type='button' class='button' onclick="window.location.href='/cs353/php/home_club.php'">
+        Home
+    </button>
+    <?php
+
 } else {
     echo "No reports yet.";
 }
---
+++ php/view_report.php
@@ -0,0 +1,34 @@
+<?php
+    include 'config.php';
+
+    $uid = $_SESSION['id'];
+    $reportID = $_POST['select'];
+
+    $result = mysqli_query($cn, "select report.id as reportID, report.date as date, report.rating as rating, 
+                report.comment as comment, scout.name as scout, footballer.name footballer 
+                from report, reports, footballer_report, scout, club, footballer
+                where report.id = '$reportID'
+                and report.id = reports.report_id 
+                and report.id = footballer_report.report_id
+                and reports.scout_id = scout.id
+                and club.id = reports.club_id
+                and club.id = '$uid'
+                and footballer_report.footballer_id = footballer.id");
+
+    $row = $result->fetch_assoc();
+    ?>
+    <button type='button' class='button' onclick="window.location.href='/cs353/php/club_reports.php'">
+    Close
+    </button> <br> <?php
+    echo "<p><b>Footballer: </b>".$row['footballer']."</p>
+        <p><b>Scout: </b>".$row['scout']."</p>
+        <p><b>Rating: </b>".$row['rating']."</p>
+        <p><b>Date: </b>".$row['date']."</p>
+        <p><b>Comment: </b>".$row['comment']."</p>";
+
+
+
+
+
+
+
