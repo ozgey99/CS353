fcd4c21134ed8f6004026ab4e5a1fa0c52240fb5
cakirfatih <55812706+cakirfatih@users.noreply.github.com>
2020-05-22 00:50:37 +0300

    Update signup_agent.php

--- php/signup_agent.php
+++ php/signup_agent.php
@@ -15,7 +15,7 @@
     <div class="input-group">
         <label>Username</label>
         <input type="text" name="username">
-    </div>
+    </div> 
     <div class="input-group">
         <label>Name</label>
         <input type="text" name="name">
@@ -27,6 +27,21 @@
     <div class="input-group">
         <label>Confirm Password</label>
         <input type="password" name="password_2">
+    </div>
+    <div>
+        <label>Please Select Your Player(s)</label>
+        <select name="footballers[]" multiple>
+            <?php 
+                $query = "SELECT id,name FROM footballer";
+                $result = mysqli_query($cn,$query);
+                while($row = mysqli_fetch_assoc($result)) {
+                    $footballer_name = $row['name'];
+                    $footballer_id = $row['id'];?>
+                    <option value="<?php echo $footballer_id;?>"><?php echo $footballer_name;?></option>
+                <?php
+                }
+            ?>
+        </select> 
     </div>
     <div class="input-group">
         <button type="submit" name="signup_agent" class="btn">Sign up</button>
