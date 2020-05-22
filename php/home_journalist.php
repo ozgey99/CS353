aed80008dfca8a99cd7f592dd5f087be352b9837
Celal Bayraktar <56126608+CelalBayraktar@users.noreply.github.com>
2020-05-22 06:48:11 +0300

    Update home_journalist.php

--- php/home_journalist.php
+++ php/home_journalist.php
@@ -13,17 +13,25 @@
     		echo "<p class='login-status'>You are logged in as Journalist!</p>";
     	}
     ?>
+    <form onclick="window.location.href='subscribe.php'">
+
+      <button type="button" name="subscribe">Subscribe</button>
+
+    </form>
+
+    <form onclick="window.location.href='notifications.php'">
+
+      <button type="button" name = "notifications">See Notifications</button>
+      
+    </form>
+
     <form action="logout_inc.php">
 
     	<button type="submit" name="logout-submit">Logout</button>
 
     </form>
 
-      <form onclick="window.location.href='subscribe.php'">
-
-          <button type="button" name="subscribe">Subscribe</button>
-
-      </form>
+      
     	
 
   </div>
